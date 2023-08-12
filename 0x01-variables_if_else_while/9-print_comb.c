3389416c42c1eda13b0c194b2d857ee64d5d10a3
LNtuli91 <luzukontuli6@gmail.com>
2023-08-12 12:49:40 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 10;)
 	{
-		for (j = i; j < 10;)
+		for (j = i; j < 10; j++)
 		{
 			putchar(j + '0');
 			if (i != 8 || j != 9)
