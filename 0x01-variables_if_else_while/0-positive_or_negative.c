ff9a5aba60b1ca928d63d48bb46a49809d686bdc
LNtuli91 <luzukontuli6@gmail.com>
2023-08-12 04:02:56 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main =  to print whether the number stored in the variable n is positive or negative
+ * main = to print whether the number stored in the variable n
  * This program will assign a random number to the variable n
  *
  * main = Entry point 
