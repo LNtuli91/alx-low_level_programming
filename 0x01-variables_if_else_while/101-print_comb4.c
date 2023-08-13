41562fb87ac2dcacf7807533bcdf0ee77755e33c
LNtuli91 <luzukontuli6@gmail.com>
2023-08-12 23:04:37 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,29 @@
-#include <unistd.h>
+#include <stdio.h>
+/**
+ * putchar_combinations - Prints all combinations of three digits from 0 to 9
+ */
 
-#include <time.h>
+void putchar_combinations(void)
+{
+	int o;
+	int p;
+	int q;
+
+	for (o = '0'; o <= '9'; o++)
+	{
+		for (p = o + 1; p <= '9'; p++)
+		{
+			for (q = p + 1; q <= '9'; q++)
+			{
+				putchar(o);
+				putchar(p);
+				putchar(q);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
 #include <stdio.h>
 
 /**
@@ -8,25 +31,9 @@
  *
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i;
-	int j;
-
-	for (i = 0; i < 100; i++)
-	{
-		for (j = i + 1; j < 100; j++)
-		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			putchar(',');
-			putchar(' ');
-		}
-	}
+	putchar_combinations();
 	return (0);
 }
 
