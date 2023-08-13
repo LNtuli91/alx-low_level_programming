cda1550b15a7744666d01e5b954950851d9ff3cf
LNtuli91 <luzukontuli6@gmail.com>
2023-08-13 04:17:08 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,17 +11,17 @@ int main(void)
 	int p;
 	int q;
 
-	for (o = '0'; o <= '9'; o++)
+	for (o = 0; o <= 7; o++)
 	{
-		for (p = o + 1; p <= '9'; p++)
+		for (p = o + 1; p <= 9; p++)
 		{
-			for (q = p + 1; q <= '9'; q++)
+			for (q = p + 1; q <= 9; q++)
 			{
-				putchar(o);
-				putchar(p);
-				putchar(q);
+				putchar(o + '0');
+				putchar(p + '0');
+				putchar(q + '0');
 
-				if (o != '7' || p != '8' || q != '9')
+				if (o != 7 || p != 8 || q != 9)
 				{
 					putchar(',');
 					putchar(' ');
