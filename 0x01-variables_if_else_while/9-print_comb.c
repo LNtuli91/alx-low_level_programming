9fd03428b3591d6aa84f9951cdfff07ee961f9a7
LNtuli91 <luzukontuli6@gmail.com>
2023-08-12 19:19:19 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; (i = 10); i++)
 	{
 		{
-			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
+			putchar(i + '0');/* print the current number*/
+			if (i <= 8)/* check if this is not the last combination*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			if (i == 9)/* check if this is the last combination*/
+			{
+				break;/* exit the inner loop*/
+			}
 		}
 	}
 	putchar('\n');
