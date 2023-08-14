6d9c4025107395e20d4114f6f8495add74e9343d
LNtuli91 <luzukontuli6@gmail.com>
2023-08-13 18:44:11 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,7 +19,6 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(c));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)\n", sizeof(f));
-	printf("\n");
+	printf("Size of a float: %lu byte(s)", sizeof(f));
 	return (0);
 }
