7f79922e25d13bfe2bb91678dd56615e889950ba
LNtuli91 <luzukontuli6@gmail.com>
2023-08-24 08:02:32 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,21 @@
 #include "main.h"
 
 /**
- *
+ * void reverse_array - reverse array of itegers
+ * @a: array
+ * @n: number ofelement of array
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n--; j++)
+	{
+		j = a[i];
+		a[j] = a[n];
+		a[n] = j;
+	}
+}
