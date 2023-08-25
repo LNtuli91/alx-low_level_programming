ce5f7d66dd4e2534eb25b517834ae59c0e9811a7
LNtuli91 <luzukontuli6@gmail.com>
2023-08-25 10:43:20 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,11 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		if (i == 0 ||
-			str[i - 1] == ' ' ||
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+
+				i++;
+
+			if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
 			str[i - 1] == ',' ||
@@ -25,18 +28,10 @@ char *cap_string(char *str)
 			str[i - 1] == '(' ||
 			str[i - 1] == ')' ||
 			str[i - 1] == '{' ||
-			str[i - 1] == '}') ||
-		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] -= ('a' - 'A');
-			}
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			str[i] += ('a' - 'A');
-		}
-		i++;
+			str[i - 1] == '}' ||
+			i == 0)
+				str[i] -= 32;
+			i++;
 	}
 	return (str);
 }
