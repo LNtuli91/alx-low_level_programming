3c4a6d22a0eb3d4b8e59cb7d6dd158c3a642e2f8
LNtuli91 <luzukontuli6@gmail.com>
2023-09-04 04:01:10 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,13 +11,23 @@
 
 char *_strdup(char *str)
 {
-	char *dup = malloc(strlen(str) + 1);
+	int i, r = 0;
+	char *dup;
 
 	if (str == NULL)
 		return (NULL);
 
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	dup = malloc(sizeof(char) * (i + 1));
+
 	if (dup == NULL)
 		return (NULL);
-	strcpy(dup, str);
+
+	for (r = 0; str[r]; r++)
+		dup[r] = str[r];
+
 	return (dup);
 }
